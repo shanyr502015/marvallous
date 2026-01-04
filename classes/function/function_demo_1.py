1b107277098f717fb18da40b4be57fbcb44941ff
nutan0897 <nutan0897@gmail.com>
2026-01-04 23:55:44 +0530

    minor

--- classes/function/function_demo_1.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("inside main")
-
-
-if __name__ == "__main__": 
-    main()
---
+++ classes/function/python_function_demo/function_demo_1.py
@@ -0,0 +1,5 @@
+def main(): # Define main function, which serves as entry point of program, when executed directly.
+    print("inside main") # Print a message indicating we are inside main function.
+
+if __name__ == "__main__": # Check if this script is being run directly (not imported as a module).  
+    main() # If so, call main function to execute code within it.   
