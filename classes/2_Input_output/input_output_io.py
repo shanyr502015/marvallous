06aaf71ad04c6263bf60fdf43c36ef1893e413c9
nutan0897 <nutan0897@gmail.com>
2026-01-08 23:47:33 +0530

    neede

--- classes/2_Input_output/input_output_io.py
+++ classes/2_Input_output/input_output_io.py
@@ -4,3 +4,31 @@ print("hello ",name)
 print("enter you age: ")
 age = input()
 print("your age is:",age)
+# -----------------------------------------------------------------------------------------------
+name = input("enter your name: ")
+print("hello ",name)
+age = input("enter you age: ")
+print("your age is:",age)
+# -----------------------------------------------------------------------------------------------
+print("enter 1 number: ")
+no1 = input()
+print(type(no1))
+print("enter 2 number: ")
+no2 = input()
+print(type(no2))
+ans = int(no1) + int(no2)
+print("addition is:", ans)
+print(type(ans))
+# -----------------------------------------------------------------------------------------------   
+print("enter 1 number: ")
+no1 =int(input()) 
+
+print("enter 2 number: ")
+no2 = int(input())
+
+print(type(no1))
+print(type(no2))
+
+ans = no1 + no2
+print("addition is:", ans)
+# -----------------------------------------------------------------------------------------------
