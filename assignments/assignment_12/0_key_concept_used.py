084e4965fb47e59a8614ba45f58c4e62dda3c341
nutan0897 <nutan0897@gmail.com>
2026-01-17 15:06:24 +0530

    assignment_12

---
+++ assignments/assignment_12/0_key_concept_used.py
@@ -0,0 +1,12 @@
+# in operator to check membership in a string
+# range() function with different parameters:
+    # range(1, num + 1) - starts from 1 to num
+    # range(num, 0, -1) - starts from num down to 1 (reverse)
+# Modulo operator (%) to find factors
+# Basic arithmetic operations (+, -, *, /)
+# end=" " to print on the same line with space separation
+
+# Notes:
+    # For problem 3, if you want integer division, use // instead of /
+    # For problem 1, the solution checks both lowercase and uppercase vowels
+    # The range(start, stop, step) function is very useful for controlling loop iteration
